a087674862805a9d60c518463a2028f478009b86
gunazavitya <93218344+gunazavitya@users.noreply.github.com>
2026-07-28 13:21:01 +0800

    fix index.js

--- index.js
+++ index.js
@@ -148,7 +148,7 @@ function createShooting(commonData){
                     return;
                 }
                 else{
-                total = ((hours - outsideHours) * rate) + (outsideHours * (rate + outsideRate)) / 2;
+                total = (((hours - outsideHours) * rate) + (outsideHours * (rate + outsideRate))) / 2;
                 }
             } else if(hours <= 0 || rate <= 0){
                 alert("Enter work hours and rate!");
